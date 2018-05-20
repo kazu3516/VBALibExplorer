6c0123c4f31a16879bc4cdd96c83b1f1346cd230
kazu3516 <kazu_3516@yahoo.co.jp>
2018-05-21 00:25:56 +0900

    NotifyFileChangedRequest/NotifyWorkspaceFolderChangedRequestの追加
    OfficeFileからFileSystemWatcherのイベントを通知するために使用する

---
+++ LibraryExplorer/Common/Request/NotifyFileChangedRequest.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using LibraryExplorer.Data;
+
+namespace LibraryExplorer.Common.Request {
+
+    #region NotifyFileChangedRequest
+
+    #region NotifyFileChangedRequestData
+    /// <summary>
+    /// ファイルが変更されたことを通知するクラスです。
+    /// </summary>
+    public class NotifyFileChangedRequestData : RequestData {
+
+        #region フィールド(メンバ変数、プロパティ、イベント)
+
+        #region TargetFile
+        private OfficeFile m_TargetFile;
+        /// <summary>
+        /// TargetFileを取得、設定します。
+        /// </summary>
+        public OfficeFile TargetFile {
+            get {
+                return this.m_TargetFile;
+            }
+            set {
+                this.m_TargetFile = value;
+            }
+        }
+        #endregion
+
+        #region FileSystemEventArgs
+        private FileSystemEventArgs m_FileSyatemEventArgs;
+        /// <summary>
+        /// FileSystemEventArgsを取得、設定します。
+        /// </summary>
+        public FileSystemEventArgs FileSyatemEventArgs {
+            get {
+                return this.m_FileSyatemEventArgs;
+            }
+            set {
+                this.m_FileSyatemEventArgs = value;
+            }
+        }
+        #endregion
+
+        #endregion
+
+        #region コンストラクタ
+        /// <summary>
+        /// NotifyFileChangedRequestDataオブジェクトの新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="targetFile"></param>
+        /// <param name="args"></param>
+        public NotifyFileChangedRequestData(OfficeFile targetFile, FileSystemEventArgs args) {
+            this.m_TargetFile = targetFile;
+            this.m_FileSyatemEventArgs = args;
+        }
+        #endregion
+
+    }
+    #endregion
+
+    #region NotifyFileChangedRequestEventArgs
+    /// <summary>
+    /// ファイルが変更されたことを通知するクラスです。
+    /// </summary>
+    public class NotifyFileChangedRequestEventArgs : RequestEventArgs {
+
+        #region フィールド(メンバ変数、プロパティ、イベント)
+
+        #endregion
+
+        #region コンストラクタ
+        /// <summary>
+        /// NotifyFileChangedRequestEventArgsオブジェクトの新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="targetFile"></param>
+        /// <param name="args"></param>
+        public NotifyFileChangedRequestEventArgs(OfficeFile targetFile, FileSystemEventArgs args) : this(targetFile,args,"") {
+        }
+        /// <summary>
+        /// NotifyFileChangedRequestEventArgsオブジェクトの新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="targetFile"></param>
+        /// <param name="args"></param>
+        /// <param name="ownerName"></param>
+        public NotifyFileChangedRequestEventArgs(OfficeFile targetFile, FileSystemEventArgs args,string ownerName) : base(new NotifyFileChangedRequestData(targetFile,args), ownerName) {
+        }
+
+        #endregion
+
+    }
+    #endregion
+
+    #endregion
+
+}
---
+++ LibraryExplorer/Common/Request/NotifyWorkspaceFolderChangedRequest.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using LibraryExplorer.Data;
+
+namespace LibraryExplorer.Common.Request {
+    
+    #region NotifyWorkspaceFolderChangedRequest
+
+    #region NotifyWorkspaceFolderChangedRequestData
+    /// <summary>
+    /// WorkSpaceFolderが変更されたことを通知するクラスです。
+    /// </summary>
+    public class NotifyWorkspaceFolderChangedRequestData : RequestData {
+
+        #region フィールド(メンバ変数、プロパティ、イベント)
+
+        #region WorkspaceFolder
+        private LibraryFolder m_WorkspaceFolder;
+        /// <summary>
+        /// WorkspaceFolderを取得、設定します。
+        /// </summary>
+        public LibraryFolder WorkspaceFolder {
+            get {
+                return this.m_WorkspaceFolder;
+            }
+            set {
+                this.m_WorkspaceFolder = value;
+            }
+        }
+        #endregion
+
+        #region FileSystemEventArgs
+        private FileSystemEventArgs m_FileSyatemEventArgs;
+        /// <summary>
+        /// FileSystemEventArgsを取得、設定します。
+        /// </summary>
+        public FileSystemEventArgs FileSyatemEventArgs {
+            get {
+                return this.m_FileSyatemEventArgs;
+            }
+            set {
+                this.m_FileSyatemEventArgs = value;
+            }
+        }
+        #endregion
+
+        #endregion
+
+        #region コンストラクタ
+        /// <summary>
+        /// NotifyWorkspaceFolderChangedRequestDataオブジェクトの新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="workspaceFolder"></param>
+        /// <param name="args"></param>
+        public NotifyWorkspaceFolderChangedRequestData(LibraryFolder workspaceFolder,FileSystemEventArgs args) {
+            this.m_WorkspaceFolder = workspaceFolder;
+            this.m_FileSyatemEventArgs = args;
+        }
+        #endregion
+
+    }
+    #endregion
+
+    #region NotifyWorkspaceFolderChangedRequestEventArgs
+    /// <summary>
+    /// WorkSpaceFolderが変更されたことを通知するクラスです。
+    /// </summary>
+    public class NotifyWorkspaceFolderChangedRequestEventArgs : RequestEventArgs {
+
+        #region フィールド(メンバ変数、プロパティ、イベント)
+
+        #endregion
+
+        #region コンストラクタ
+        /// <summary>
+        /// NotifyWorkspaceFolderChangedRequestEventArgsオブジェクトの新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="workspaceFolder"></param>
+        /// <param name="args"></param>
+        public NotifyWorkspaceFolderChangedRequestEventArgs(LibraryFolder workspaceFolder, FileSystemEventArgs args) : this(workspaceFolder,args,"") {
+        }
+        /// <summary>
+        /// NotifyWorkspaceFolderChangedRequestEventArgsオブジェクトの新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="workspaceFolder"></param>
+        /// <param name="args"></param>
+        /// <param name="ownerName"></param>
+        public NotifyWorkspaceFolderChangedRequestEventArgs(LibraryFolder workspaceFolder, FileSystemEventArgs args, string ownerName) : base(new NotifyWorkspaceFolderChangedRequestData(workspaceFolder,args), ownerName) {
+        }
+
+        #endregion
+
+    }
+    #endregion
+
+    #endregion
+
+}
